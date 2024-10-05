ed2a516e5f6c414e23609a3e484b0f537c543f24
Harumi ✨ <144253276+harumi7@users.noreply.github.com>
2024-10-05 09:28:45 -0300

    Small details fixed

--- script.js
+++ script.js
@@ -2,32 +2,32 @@ function changeColorTheme(colors) {
     const randomIndex = Math.floor(Math.random() * colors.length);
     const color = colors[randomIndex];
 
-    // Alterar cor de fundo do elemento main
+    // Change background color of the element "main"
     document.getElementById("main").style.backgroundColor = color;
 
-    // Alterar cor de fundo do botão de tema
+    // Change background color of the button that changes the color theme
     const botao = document.getElementById("change-color-theme");
     botao.style.backgroundColor = color;
 
-    // Alterar cor de fundo do botão submit nos eventos de mouse
-    const buttonSubmit = document.getElementById('button-submit');
-    buttonSubmit.addEventListener('mouseover', function() {
+    // Change background color of the submit button (form/contact section)
+    const buttonSubmit = document.getElementById("button-submit");
+    buttonSubmit.addEventListener("mouseover", function() {
         this.style.backgroundColor = color;
     });
 
-    buttonSubmit.addEventListener('mouseout', function() {
-        this.style.backgroundColor = '#fff';
+    buttonSubmit.addEventListener("mouseout", function() {
+        this.style.backgroundColor = "white";
     });
 
-    // Alterar cor de texto das opções de menu nos eventos de mouse
+    // When hover, the menu text color will change accordingly to the selected color theme
     const opcoesMenu = document.getElementsByClassName('menu-option');
     for (let i = 0; i < opcoesMenu.length; i++) {
-        opcoesMenu[i].addEventListener('mouseover', function() {
+        opcoesMenu[i].addEventListener("mouseover", function() {
             this.style.color = color;
         });
 
-        opcoesMenu[i].addEventListener('mouseout', function() {
-            this.style.color = '#000';
+        opcoesMenu[i].addEventListener("mouseout", function() {
+            this.style.color = "black";
         });
     }
 }
@@ -36,6 +36,7 @@ var i = 0;
 var txt = "Information Technology student";
 var speed = 120;
 
+// Write the string letter by letter
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("typewriter").innerHTML += txt.charAt(i);
@@ -44,12 +45,14 @@ function typeWriter() {
   }
 }
 
+// When clicked, the user returns to the top of the page
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
+// Get the actual year, this way, the copyright year is automatically modified
 function getYear() {
     const year = new Date().getFullYear();
-    document.getElementById('year').textContent = year;
+    document.getElementById("year").textContent = year;
 }
